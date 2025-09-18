c871f668734af79029093a88375c116345f9d954
Alexander Höllinger <a.hoellinger@ewemed.at>
2025-09-18 09:10:06 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+const passwordLength = 10;
+
+function generatePassword() {
+    let password = '';
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    return password;
+}
+console.log("Generated Password: " + generatePassword());
